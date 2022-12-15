87510d82d8ac62fb88f58b1e736310dcd7639520
CoolStar <coolstarorganization@gmail.com>
2022-12-15 01:54:18 -0800

    Allow disabling in device manager

--- crossensors/crossensors.cpp
+++ crossensors/crossensors.cpp
@@ -513,6 +513,14 @@ CrosSensorsEvtDeviceAdd(
 		return status;
 	}
 
+	{
+		WDF_DEVICE_STATE deviceState;
+		WDF_DEVICE_STATE_INIT(&deviceState);
+
+		deviceState.NotDisableable = WdfFalse;
+		WdfDeviceSetDeviceState(device, &deviceState);
+	}
+
 	WDF_IO_QUEUE_CONFIG_INIT_DEFAULT_QUEUE(&queueConfig, WdfIoQueueDispatchParallel);
 
 	queueConfig.EvtIoInternalDeviceControl = CrosSensorsEvtInternalDeviceControl;
